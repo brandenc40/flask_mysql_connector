c1d4941b171904d9c52f371508ee378aaebafb75
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2020-05-26 23:29:16 -0500

    added example script

--- README.md
+++ README.md
@@ -11,13 +11,13 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DATABASE'] = 'sys'
 mysql = MySQL(app)
 
-_example_sql = 'select * from sys.user_summary'
+EXAMPLE_SQL = 'select * from sys.user_summary'
 
 
 @app.route('/new_cursor')
 def new_cursor():
     cur = mysql.new_cursor(dictionary=True)
-    cur.execute(_example_sql)
+    cur.execute(EXAMPLE_SQL)
     output = cur.fetchall()
     return str(output)
 
@@ -26,14 +26,14 @@ def new_cursor():
 def connection():
     conn = mysql.connection
     cur = conn.cursor()
-    cur.execute(_example_sql)
+    cur.execute(EXAMPLE_SQL)
     output = cur.fetchall()
     return str(output)
 
 
 @app.route('/easy_execute')
 def easy_execute():
-    df = mysql.execute_sql(_example_sql, to_pandas=True)
+    df = mysql.execute_sql(EXAMPLE_SQL, to_pandas=True)
     return str(df.to_dict())
 
 
--- examples/example_app.py
+++ examples/example_app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import sys
+sys.path.append('../flask_mysql_connector')
 from flask_mysql_connector import MySQL
 
 app = Flask(__name__)
@@ -6,13 +8,13 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DATABASE'] = 'sys'
 mysql = MySQL(app)
 
-_example_sql = 'select * from sys.user_summary'
+EXAMPLE_SQL = 'select * from sys.user_summary'
 
 
-@app.route('/new_cursor')
+@app.route('/')
 def new_cursor():
     cur = mysql.new_cursor(dictionary=True)
-    cur.execute(_example_sql)
+    cur.execute(EXAMPLE_SQL)
     output = cur.fetchall()
     return str(output)
 
@@ -21,14 +23,14 @@ def new_cursor():
 def connection():
     conn = mysql.connection
     cur = conn.cursor()
-    cur.execute(_example_sql)
+    cur.execute(EXAMPLE_SQL)
     output = cur.fetchall()
     return str(output)
 
 
 @app.route('/easy_execute')
 def easy_execute():
-    df = mysql.execute_sql(_example_sql, to_pandas=True)
+    df = mysql.execute_sql(EXAMPLE_SQL, to_pandas=True)
     return str(df.to_dict())
 
 
